764116027273e8831aafe6a80bb766ec8e9488f9
Nikita "Tehnick <71207932+tehnickge@users.noreply.github.com>
2024-10-05 22:29:41 +0300

    init session, make session wrapper, create credential

---
+++ next-chat/lib/authOptions.ts
@@ -0,0 +1,29 @@
+import { NextAuthOptions } from "next-auth";
+import CredentialsProvider from "next-auth/providers/credentials";
+
+export const authOptions: NextAuthOptions = {
+  providers: [
+    CredentialsProvider({
+      // The name to display on the sign in form (e.g. "Sign in with...")
+      name: "Credentials",
+      // `credentials` is used to generate a form on the sign in page.
+      // You can specify which fields should be submitted, by adding keys to the `credentials` object.
+      // e.g. domain, username, password, 2FA token, etc.
+      // You can pass any HTML attribute to the <input> tag through the object.
+      credentials: {
+        username: { label: "Username", type: "text", placeholder: "jsmith" },
+        password: { label: "Password", type: "password" },
+      },
+      // eslint-disable-next-line @typescript-eslint/no-unused-vars
+      async authorize(credentials, req) {
+        const user = { id: "1", name: "J Smith", email: "jsmith@example.com" };
+
+        if (user) {
+          return user;
+        } else {
+          return null;
+        }
+      },
+    }),
+  ],
+};
--- next-chat/src/app/page.tsx
+++ next-chat/src/app/page.tsx
@@ -1,8 +1,13 @@
 "use client";
+import { useSession } from "next-auth/react";
 import styled from "styled-components";
 
 export default function Home() {
+  const session = useSession();
+  console.log(session.status);
   return <Main></Main>;
 }
 
-const Main = styled.div``;
+const Main = styled.div`
+  color: black;
+`;
