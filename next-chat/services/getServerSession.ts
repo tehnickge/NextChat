ab2fbea11acfd2816989054f47bd9177ae62e811
Nikita "Tehnick <71207932+tehnickge@users.noreply.github.com>
2024-10-07 11:25:34 +0300

    make ServerServerSessionProps

---
+++ next-chat/services/getServerSession.ts
@@ -0,0 +1,24 @@
+import { getServerSession } from "next-auth";
+import { authOptions } from "../lib/authOptions"; // Путь к вашему файлу next-auth
+import { GetServerSidePropsContext } from "next";
+
+export async function getServerSideProps(context : GetServerSidePropsContext) {
+  const session = await getServerSession(context.req, context.res, authOptions);
+
+  if (!session) {
+    // Если пользователь не авторизован, можно перенаправить его на страницу логина или обработать это иначе
+    return {
+      redirect: {
+        destination: '/auth/login',
+        permanent: false,
+      },
+    };
+  }
+
+  // Если пользователь авторизован, передаем данные дальше
+  return {
+    props: {
+      user: session.user,
+    },
+  };
+}
