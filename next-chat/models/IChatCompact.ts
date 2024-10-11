5d667818c927e1af9693f4c1c5f7d80397f98535
Nikita "Tehnick <71207932+tehnickge@users.noreply.github.com>
2024-10-11 12:03:55 +0300

    add and fix models

---
+++ next-chat/models/IChatCompact.ts
@@ -0,0 +1,4 @@
+export interface IChatCompact {
+  id: number;
+  isGroup: boolean;
+}
--- next-chat/models/IUser.ts
+++ next-chat/models/IUser.ts
@@ -1,6 +1,6 @@
 export default interface IUser {
   id?: number;
-  name: string;
+  username: string;
   password: string;
   email: string;
   createdAt?: Date;
